0097ff33539ba31604ec3426e0444e04cd919cdb
Nadeem <nadeempasha9889@gmail.com>
2026-04-23 21:53:01 -0400

    solved a hard dsu problem

--- Graph/DisjointUnionSet/CheckingExistenceofEdgeLengthLimitedPaths.py
+++ Graph/DisjointUnionSet/CheckingExistenceofEdgeLengthLimitedPaths.py
@@ -1,1 +1,26 @@
-#Figured out the concept behind this problem
+
+def distanceLimitedPathsExist(n, edgeList, queries):
+    parent=list(range(n))
+    result=[False]*len(queries)
+    edgeList.sort(key=lambda x: x[2])
+    sorted_queries=sorted([(q, i) for i, q in enumerate(queries)],key=lambda x: x[0][2])
+    def findParent(x):
+        if parent[x]!=x:
+            parent[x]=findParent(parent[x])
+        return parent[x]
+    def union(x,y):
+        rx=findParent(x)
+        ry=findParent(y)
+        if rx!=ry:
+            parent[rx]=ry
+    start=0
+    for q,i in sorted_queries:
+        u,v,w = q
+        while start<len(edgeList) and edgeList[start][2]<w:
+            union(edgeList[start][0],edgeList[start][1])
+            start+=1
+        result[i]= findParent(u)==findParent(v)
+    return result
+
+        
+        
