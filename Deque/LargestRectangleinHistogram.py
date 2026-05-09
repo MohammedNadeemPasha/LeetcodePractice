c5e821f2c34c6c7194da590e37b6a53dde5543d5
Nadeem <nadeempasha9889@gmail.com>
2026-05-09 13:13:28 -0400

    solved a hard deque type problem

--- Deque/LargestRectangleinHistogram.py
+++ Deque/LargestRectangleinHistogram.py
@@ -23,3 +23,22 @@
 #   - Update max area.
 # - Add a 0 height at the end to force all bars to be processed.
 # - Return max area.
+
+from collections import deque
+def largestRectangleArea(heights) :
+    max_area=0
+    heights.append(0)
+    q=deque()
+    for i,height in enumerate(heights):
+        if len(q) and  height<heights[q[-1]]:
+            while len(q) and heights[q[-1]]>height:
+                prev_index=q.pop()
+                width=0
+                if not len(q):
+                    width=i
+                else:
+                    width=i-q[-1]-1
+                max_area=max(max_area,width*heights[prev_index])
+        q.append(i)
+    return max_area
+print(largestRectangleArea([2,1,5,6,2,3])) #O/P -> 10
